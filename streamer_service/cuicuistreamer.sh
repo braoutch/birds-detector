767424de9eded39838f3741251263e6dedb5734c
Antoine Lassagne <antoinelassagne@gmail.com>
2023-10-14 23:30:43 +0200

    finalize compute app

--- streamer_service/cuicuistreamer.sh
+++ streamer_service/cuicuistreamer.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-libcamera-vid -t 0 --width 1920 --height 1080 --codec h264 --inline --listen -o tcp://0.0.0.0:8888
+libcamera-vid -t 0 --width 960 --height 540 --codec h264 --inline --listen -o tcp://0.0.0.0:8080
+# libcamera-vid -t 0 --width 960 --height 540 --inline -o - | cvlc -vvv stream:///dev/stdin --sout '#rtp{sdp=rtsp://:8080/}' :demux=h264 --sout-rtsp-user braoutch --sout-rtsp-pwd aeddqkmo
+# libcamera-vid -t 0 --width 960 --height 540 --inline  -o udp://0.0.0.0:8080
