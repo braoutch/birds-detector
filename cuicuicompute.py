0d163111739ddad8a93873fd0f95d280c7ba8f67
Antoine Lassagne <antoinelassagne@gmail.com>
2023-10-14 19:41:43 +0200

    wip

--- cuicuicompute.py
+++ cuicuicompute.py
@@ -108,14 +108,14 @@ class ImageSender:
         self.is_bird_here = False
         self.last_time_sent = datetime.now()
 
-    def sendImageWithinLimits(self, number_of_objects: int, image):
+    def sendImageWithinLimits(self, number_of_objects: int, message: str, image):
       print("Sending (or not)", number_of_objects, "detections")
       if number_of_objects <= 0:
         self.is_bird_here = False
         return
       
       elif not self.is_bird_here:
-        self.telegram.send_message("Bird just arrived !")
+        self.telegram.send_message("Bird just arrived ! " + message)
         self.telegram.send_photo(image)
         self.is_bird_here = True
 
@@ -131,7 +131,7 @@ def main():
     model_labels = {0: u'__background__', 1: u'person', 2: u'bicycle',3: u'car', 4: u'motorcycle', 5: u'airplane', 6: u'bus', 7: u'train', 8: u'truck', 9: u'boat', 10: u'traffic light', 11: u'fire hydrant', 12: u'stop sign', 13: u'parking meter', 14: u'bench', 15: u'bird', 16: u'cat', 17: u'dog', 18: u'horse', 19: u'sheep', 20: u'cow', 21: u'elephant', 22: u'bear', 23: u'zebra', 24: u'giraffe', 25: u'backpack', 26: u'umbrella', 27: u'handbag', 28: u'tie', 29: u'suitcase', 30: u'frisbee', 31: u'skis', 32: u'snowboard', 33: u'sports ball', 34: u'kite', 35: u'baseball bat', 36: u'baseball glove', 37: u'skateboard', 38: u'surfboard', 39: u'tennis racket', 40: u'bottle', 41: u'wine glass', 42: u'cup', 43: u'fork', 44: u'knife', 45: u'spoon', 46: u'bowl', 47: u'banana', 48: u'apple', 49: u'sandwich', 50: u'orange', 51: u'broccoli', 52: u'carrot', 53: u'hot dog', 54: u'pizza', 55: u'donut', 56: u'cake', 57: u'chair', 58: u'couch', 59: u'potted plant', 60: u'bed', 61: u'dining table', 62: u'toilet', 63: u'tv', 64: u'laptop', 65: u'mouse', 66: u'remote', 67: u'keyboard', 68: u'cell phone', 69: u'microwave', 70: u'oven', 71: u'toaster', 72: u'sink', 73: u'refrigerator', 74: u'book', 75: u'clock', 76: u'vase', 77: u'scissors', 78: u'teddy bear', 79: u'hair drier', 80: u'toothbrush'}
     model = YOLO('yolov5su.pt')  # pretrained YOLOv8n model
     url = 'tcp://192.168.50.212:8888/' 
-    url = 'sample.mp4' 
+    # url = 'sample.mp4' 
 
     image_sender = ImageSender()
 
@@ -166,9 +166,8 @@ def main():
               label = get_label(box)
               labels.append(label)
               if 'bird' in label:
-                  image_sender.sendImageWithinLimits(count, frame)
+                  image_sender.sendImageWithinLimits(count, label, frame)
 
-            # print(boxes.data)
         print(len(labels), "label:", labels)
 
         plot_bboxes(frame, boxes.data, score=True)
