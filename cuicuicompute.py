56d5dbba2073c8ca5130c590da29271a217d3f51
Antoine Lassagne <antoinelassagne@gmail.com>
2023-10-14 16:59:54 +0200

    fixes

--- cuicuicompute.py
+++ cuicuicompute.py
@@ -8,6 +8,7 @@ from PIL import Image, ImageDraw
 import requests
 # from picamera2 import Picamera2
 from datetime import datetime, timedelta
+import time 
 
 def box_label(image, box, label='', color=(128, 128, 128), txt_color=(255, 255, 255)):
   lw = max(round(sum(image.shape) / 2 * 0.003), 2)
@@ -127,21 +128,30 @@ class ImageSender:
 
 def main():
     print("Hello World!")
+    model_labels = {0: u'__background__', 1: u'person', 2: u'bicycle',3: u'car', 4: u'motorcycle', 5: u'airplane', 6: u'bus', 7: u'train', 8: u'truck', 9: u'boat', 10: u'traffic light', 11: u'fire hydrant', 12: u'stop sign', 13: u'parking meter', 14: u'bench', 15: u'bird', 16: u'cat', 17: u'dog', 18: u'horse', 19: u'sheep', 20: u'cow', 21: u'elephant', 22: u'bear', 23: u'zebra', 24: u'giraffe', 25: u'backpack', 26: u'umbrella', 27: u'handbag', 28: u'tie', 29: u'suitcase', 30: u'frisbee', 31: u'skis', 32: u'snowboard', 33: u'sports ball', 34: u'kite', 35: u'baseball bat', 36: u'baseball glove', 37: u'skateboard', 38: u'surfboard', 39: u'tennis racket', 40: u'bottle', 41: u'wine glass', 42: u'cup', 43: u'fork', 44: u'knife', 45: u'spoon', 46: u'bowl', 47: u'banana', 48: u'apple', 49: u'sandwich', 50: u'orange', 51: u'broccoli', 52: u'carrot', 53: u'hot dog', 54: u'pizza', 55: u'donut', 56: u'cake', 57: u'chair', 58: u'couch', 59: u'potted plant', 60: u'bed', 61: u'dining table', 62: u'toilet', 63: u'tv', 64: u'laptop', 65: u'mouse', 66: u'remote', 67: u'keyboard', 68: u'cell phone', 69: u'microwave', 70: u'oven', 71: u'toaster', 72: u'sink', 73: u'refrigerator', 74: u'book', 75: u'clock', 76: u'vase', 77: u'scissors', 78: u'teddy bear', 79: u'hair drier', 80: u'toothbrush'}
     model = YOLO('yolov5su.pt')  # pretrained YOLOv8n model
     url = 'tcp://192.168.50.212:8888/' 
-    cap = cv2.VideoCapture(url, cv2.CAP_FFMPEG)
+    url = 'sample.mp4' 
 
     image_sender = ImageSender()
 
     os.environ['OPENCV_FFMPEG_CAPTURE_OPTIONS'] = 'rtsp_transport;udp'
+    
+    # cap = cv2.VideoCapture(url, cv2.CAP_FFMPEG)
+    # pre = 0
+    # while pre < 240:
+    #    pre = pre + 1
+    #    ret, frame = cap.read()
 
     while True:
         print("Detection attempt...")
+        cap = cv2.VideoCapture(url, cv2.CAP_FFMPEG)
+
         ret, frame = cap.read()
-        # cv2.imshow('RTSP stream', frame)
+        cv2.imshow('RTSP stream', frame)
         cv2.waitKey(1)
         # Run batched inference on a list of images
-        results = model(frame)  # return a list of Results objects
+        results = model.predict(frame, verbose=False)  # return a list of Results objects
 
         count = 0
         labels = []
@@ -152,21 +162,25 @@ def main():
             masks = result.masks  # Masks object for segmentation masks outputs
             keypoints = result.keypoints  # Keypoints object for pose outputs
             probs = result.probs  # Probs object for classification outputs
-            for box in boxes:
-              label = get_label(box, score=True)
+            for box in boxes.data:
+              label = get_label(box)
               labels.append(label)
-              if label.contains("Bird"):
+              if 'bird' in label:
                   image_sender.sendImageWithinLimits(count, frame)
 
             # print(boxes.data)
         print(len(labels), "label:", labels)
 
-        # plot_bboxes(frame, boxes.data, score=True)
+        plot_bboxes(frame, boxes.data, score=True)
+        cv2.waitKey(1)
 
         if cv2.waitKey(1) == 27:
             break
+        
+        cap.release()
+        time.sleep(2)
 
-    cap.release()
+
     cv2.destroyAllWindows()
 
 if __name__ == "__main__":
